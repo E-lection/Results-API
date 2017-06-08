b09fb1464bcb4df8d33fec24d746cec1b108b294
Oliver Stenbom <oliver@stenbom.eu>
2017-06-08 15:19:00 +0200

    Accepting faulty JSON in /vote, candidate_first_name -> first_name and same for laster

--- results/views.py
+++ results/views.py
@@ -11,16 +11,22 @@ def index(request):
 def vote(request):
     if request.method == 'POST':
         vote_data = json.loads(request.body)
-        constituency = vote_data['constituency']
-        party = vote_data['party']
-        candidate_first_name = vote_data['candidate_first_name']
-        candidate_last_name = vote_data['candidate_last_name']
-        if constituency and party and candidate_last_name and candidate_first_name:
-            vote_object = Vote(constituency=constituency,
-                               party=party,
-                               candidate_first_name=candidate_first_name,
-                               candidate_last_name=candidate_last_name)
-            vote_object.save()
-            return JsonResponse({'success': True})
+        if (hasattr(vote_data, 'constituency') and
+                hasattr(vote_data, 'party') and
+                hasattr(vote_data, 'first_name') and
+                hasattr(vote_data, 'last_name')):
+
+            constituency = vote_data['constituency']
+            party = vote_data['party']
+            candidate_first_name = vote_data['first_name']
+            candidate_last_name = vote_data['last_name']
+
+            if constituency and party and candidate_last_name and candidate_first_name:
+                vote_object = Vote(constituency=constituency,
+                                   party=party,
+                                   candidate_first_name=candidate_first_name,
+                                   candidate_last_name=candidate_last_name)
+                vote_object.save()
+                return JsonResponse({'success': True})
 
     return JsonResponse({'success': False})
