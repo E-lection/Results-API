dd2897797f3ad2f6fdbd9e566d71b1b9996ec497
William Ray <wor15@ic.ac.uk>
2017-06-13 18:52:07 +0100

    Exposed some extra API end points for testing

--- results/urls.py
+++ results/urls.py
@@ -6,5 +6,7 @@ app_name = 'results'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^vote/$', views.vote, name='vote'),
+    url(r'^vote_script/$', views.vote_script, name='vote_script'),
+    url(r'^delete_votes/$', views.delete_votes, name='delete_votes'),
     url(r'^outcome/$', views.outcome, name='outcome')
 ]
--- results/views.py
+++ results/views.py
@@ -39,6 +39,35 @@ def vote(request):
     return JsonResponse({'success': False,
                          'error' : 'Missing input data'})
 
+@csrf_exempt
+def vote_script(request):
+    if request.method == 'POST':
+        vote_data = json.loads(request.body)
+
+        if all (k in vote_data for k in ('constituency', 'party', 'first_name', 'last_name')):
+            constituency = vote_data['constituency']
+            party = vote_data['party']
+            candidate_first_name = vote_data['first_name']
+            candidate_last_name = vote_data['last_name']
+
+            if constituency and party and candidate_last_name and candidate_first_name:
+                vote_object = Vote(constituency=constituency,
+                                   party=party,
+                                   candidate_first_name=candidate_first_name,
+                                   candidate_last_name=candidate_last_name)
+                vote_object.save()
+                return JsonResponse({'success': True,
+                                     'error' : None})
+
+
+
+    return JsonResponse({'success': False,
+                         'error' : 'Missing input data'})
+
+def delete_votes(request):
+    Vote.objects.all().delete()
+    return JsonResponse({'success' : True})
+
 
 def outcome(request):
     votes = count_and_package_all_votes()
