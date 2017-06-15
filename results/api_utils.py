c6352716dcbcd82583f54998430603d01a60e4f6
William Ray <wor15@ic.ac.uk>
2017-06-15 14:31:21 +0100

    Reverted URLs from localhost

--- results/api_utils.py
+++ results/api_utils.py
@@ -4,7 +4,7 @@ import json
 
 from api_key_verification import RESULTS_KEY
 
-PIN_VERIFICATION_URL = 'http://127.0.0.1:7000/verify_pin_code_and_make_ineligible'
+PIN_VERIFICATION_URL = 'http://pins.eelection.co.uk/verify_pin_code_and_make_ineligible'
 
 def verify_pin_and_make_ineligible(station_id, pin_code):
     url = PIN_VERIFICATION_URL + '/station_id/' + urllib.quote(str(station_id)) + '/pin_code/' + urllib.quote(str(pin_code))
